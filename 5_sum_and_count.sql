b79bf05b5b4633ca488d26b8ba89a8aaa81a38a0
PlaustralCL <plaustralcl@gmail.com>
2021-12-25 17:13:34 -0500

    Complete 5_sum_and_count

---
+++ 5_sum_and_count.sql
@@ -0,0 +1,33 @@
+-- https://sqlzoo.net/wiki/SUM_and_COUNT
+
+-- Exercise 3
+SELECT SUM(gdp)
+FROM world
+WHERE continent = 'Africa';
+
+-- Exercise 4
+SELECT COUNT(name)
+FROM world
+WHERE area >= 1000000;
+
+-- Exercise 5
+SELECT SUM(population)
+FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- Exercise 6
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+-- Exercise 7
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+-- Exercise 8
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
